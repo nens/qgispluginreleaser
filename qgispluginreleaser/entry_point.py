b0cfaa8e782e4006e10258e85c466809a63da051
Reinout van Rees <reinout@vanrees.org>
2025-06-18 09:32:36 +0200

    precommit/ruff

--- qgispluginreleaser/entry_point.py
+++ qgispluginreleaser/entry_point.py
@@ -1,11 +1,9 @@
-from __future__ import unicode_literals
-
+import codecs
 import glob
 import os
 import shutil
 import subprocess
 import time
-import codecs
 
 
 def find_metadata_file():
@@ -13,15 +11,15 @@ def find_metadata_file():
 
     Will look into current folder and one subdirectory deeper.
     """
-    path = glob.glob('metadata.txt')
-    path += glob.glob('*/metadata.txt')
+    path = glob.glob("metadata.txt")
+    path += glob.glob("*/metadata.txt")
     if len(path) == 1:
-        if 'qgisMinimumVersion' in codecs.open(path[0], 'r', 'utf-8').read():
+        if "qgisMinimumVersion" in codecs.open(path[0], "r", "utf-8").read():
             return path[0]
     elif len(path) > 1:
         print("Multiple 'metadata.txt files have been found: %s" % path)
         print("Using the first found file: %s" % path[0])
-        if 'qgisMinimumVersion' in codecs.open(path[0], 'r', 'utf-8').read():
+        if "qgisMinimumVersion" in codecs.open(path[0], "r", "utf-8").read():
             return path[0]
 
 
@@ -48,11 +46,11 @@ def create_zipfile(context):
     if not metadata_file:
         return
     # Create a zipfile.
-    subprocess.call(['make', 'zip'])
-    for zipfile in glob.glob('*.zip'):
-        first_part = zipfile.split('.')[0]
-        new_name = "%s.%s.zip" % (first_part, context['version'])
-        target = os.path.join(context['workingdir'], new_name)
+    subprocess.call(["make", "zip"])
+    for zipfile in glob.glob("*.zip"):
+        first_part = zipfile.split(".")[0]
+        new_name = "%s.%s.zip" % (first_part, context["version"])
+        target = os.path.join(context["workingdir"], new_name)
         shutil.copy(zipfile, target)
         print("Copied %s to %s" % (zipfile, target))
 
@@ -67,10 +65,10 @@ def fix_version(context):
     metadata_file = find_metadata_file()
     if not metadata_file:
         return
-    lines = codecs.open(metadata_file, 'r', 'utf-8').readlines()
+    lines = codecs.open(metadata_file, "r", "utf-8").readlines()
     for index, line in enumerate(lines):
-        if line.startswith('version'):
-            new_line = 'version=%s\n' % context['new_version']
+        if line.startswith("version"):
+            new_line = "version=%s\n" % context["new_version"]
             lines[index] = new_line
     time.sleep(1)
-    codecs.open(metadata_file, 'w', 'utf-8').writelines(lines)
+    codecs.open(metadata_file, "w", "utf-8").writelines(lines)
--- qgispluginreleaser/test.py
+++ qgispluginreleaser/test.py
@@ -1,17 +1,18 @@
-from qgispluginreleaser.entry_point import create_zipfile
-from qgispluginreleaser.entry_point import find_metadata_file
-from unittest import TestCase
+from unittest import TestCase, mock
 
-import mock
 import pkg_resources
+
+from qgispluginreleaser.entry_point import create_zipfile, find_metadata_file
 
 
 class InstallationTestCase(TestCase):
-
     def test_entry_point_available(self):
-        entry_points = list(pkg_resources.iter_entry_points(
-            group='zest.releaser.releaser.after_checkout'))
-        self.assertTrue('qgispluginreleaser.entry_point' in str(entry_points))
+        entry_points = list(
+            pkg_resources.iter_entry_points(
+                group="zest.releaser.releaser.after_checkout"
+            )
+        )
+        self.assertTrue("qgispluginreleaser.entry_point" in str(entry_points))
 
     def test_metadata_file(self):
         # Should return false for us.
@@ -27,31 +28,26 @@ def return_metadata():
 
 
 class EntryPointTestCase(TestCase):
-
     def setUp(self):
         self.patcher = mock.patch(
-            'qgispluginreleaser.entry_point.metadata_file',
-            return_metadata)
+            "qgispluginreleaser.entry_point.metadata_file", return_metadata
+        )
         self.patcher.start()
 
     def tearDown(self):
         self.patcher.stop()
 
     def test_makefile_call(self):
-        with mock.patch('subprocess.call') as mocked:
+        with mock.patch("subprocess.call") as mocked:
             create_zipfile({})
             self.assertTrue(mocked.called)
 
     def test_ziprename(self):
-
         def mock_glob(something):
-            return ['something.zip']
+            return ["something.zip"]
 
-        with mock.patch('subprocess.call'):
-            with mock.patch('glob.glob', mock_glob):
-                with mock.patch('shutil.copy') as mocked:
-                    create_zipfile({'version': '1.0',
-                         'workingdir': '/tmp'})
-                    mocked.assert_called_with(
-                        'something.zip',
-                        '/tmp/something.1.0.zip')
+        with mock.patch("subprocess.call"):
+            with mock.patch("glob.glob", mock_glob):
+                with mock.patch("shutil.copy") as mocked:
+                    create_zipfile({"version": "1.0", "workingdir": "/tmp"})
+                    mocked.assert_called_with("something.zip", "/tmp/something.1.0.zip")
